df31c669e32d009695648eb5a9abb40e87094691
sofia <sofia.yu@voyageone.cn>
2016-06-21 19:19:52 +0800

    add some pages

--- eve-manager-front/src/app/eve-manager-front.component.ts
+++ eve-manager-front/src/app/eve-manager-front.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Routes, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
+import { DashboardComponent } from './pages/+dashboard/dashboard.component';
+import { LoginComponent } from './pages/+login/login.component';
 
 @Component({
   moduleId: module.id,
   selector: 'eve-manager-front-app',
   templateUrl: 'eve-manager-front.component.html',
-  styleUrls: ['eve-manager-front.component.css']
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+
+@Routes([
+	{ path: '/dashboard', component: DashboardComponent },
+	{ path: '/login', component: LoginComponent },
+])
 export class EveManagerFrontAppComponent {
   title = 'eve-manager-front works!';
 }
--- eve-manager-front/src/main.ts
+++ eve-manager-front/src/main.ts
@@ -1,4 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { RootRouter } from 'angular2/src/router/router';
+import { Router } from 'angular2/router';
+
 import { enableProdMode } from '@angular/core';
 import { EveManagerFrontAppComponent, environment } from './app/';
 
